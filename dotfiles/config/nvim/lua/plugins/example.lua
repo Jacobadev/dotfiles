4b00fa01ead6b96acbf3181e1c90739289a40b34
jacobadev <junior1232131@gmail.com>
2024-03-02 00:47:09 -0300

    automatic scheduled task

--- dotfiles/config/nvim/lua/plugins/example.lua
+++ dotfiles/config/nvim/lua/plugins/example.lua
@@ -2,7 +2,7 @@
 -- stylua: ignore
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
--- In your plugin files, you can:
+-- In your plugin files, you can
 -- * add extra plugins
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
