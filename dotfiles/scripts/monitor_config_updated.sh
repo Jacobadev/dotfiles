b16a34b959e6d6800bce656030ec578ff859517f
jacobadev <junior1232131@gmail.com>
2024-04-03 08:12:30 -0300

    automatic scheduled task

--- dotfiles/scripts/monitor_config_updated.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output VGA-0 --primary --mode 1280x1024 --pos 1920x225 --rotate normal --output VGA-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off --output None-1-1 --off
