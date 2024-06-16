4ba979ec0a1c0a1fad52d1e3631ecbeb86fbaa18
jacobadev <junior1232131@gmail.com>
2024-06-16 05:52:40 -0300

    automatic scheduled task

--- dotfiles/config/nvim/lua/plugins/cmp.lua
+++ dotfiles/config/nvim/lua/plugins/cmp.lua
@@ -1,189 +1,198 @@
 return {
-    --
-    {
-        "L3MON4D3/LuaSnip",
-        keys = function()
-            return {}
-        end,
+  --
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function()
+      return {}
+    end,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      {
+        "js-everts/cmp-tailwind-colors",
+        opts = {},
+      },
     },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
 
-    {
-        "hrsh7th/nvim-cmp",
+      local kind_icons = {
+        Text = "",
+        Method = "m",
+        Function = "󰊕",
+        Constructor = "",
+        Field = "",
+        Variable = "",
+        Class = "",
+        Interface = "",
+        Module = "",
+        Property = " ",
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰌋",
+        Snippet = "",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰏿",
+        Struct = "",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = " ",
+      }
 
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-cmdline",
-            "hrsh7th/cmp-nvim-lsp-signature-help",
-            "saadparwaiz1/cmp_luasnip",
-            "L3MON4D3/LuaSnip",
-            {
-                "js-everts/cmp-tailwind-colors",
-                opts = {},
-            },
+      local buffer_option = {
+        -- Complete from all visible buffers (splits)
+        get_bufnrs = function()
+          local bufs = {}
+          for _, win in ipairs(vim.api.nvim_list_wins()) do
+            bufs[vim.api.nvim_win_get_buf(win)] = true
+          end
+          return vim.tbl_keys(bufs)
+        end,
+      }
+      ---@diagnostic disable
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
         },
-        config = function()
-            local cmp = require("cmp")
-            local luasnip = require("luasnip")
+        sources = cmp.config.sources({
+          {
+            name = "nvim_lsp",
+            priority = 10,
+            entry_filter = limit_lsp_types,
+          },
+          {
+            name = "luasnip",
+            priority = 7,
+            max_item_count = 5,
+          },
+          {
+            name = "buffer",
+            priority = 7,
+            keyword_length = 5,
+            max_item_count = 10,
+            option = buffer_option,
+          },
+          { name = "nvim_lua", priority = 5 },
+          { name = "path", priority = 4 },
+        }),
+        duplicates = {
+          nvim_lsp = 1,
+          luasnip = 1,
+          buffer = 1,
+          path = 1,
+        },
 
-            local kind_icons = {
-                Text = "",
-                Method = "m",
-                Function = "󰊕",
-                Constructor = "",
-                Field = "",
-                Variable = "",
-                Class = "",
-                Interface = "",
-                Module = "",
-                Property = " ",
-                Unit = "",
-                Value = "󰎠",
-                Enum = "",
-                Keyword = "󰌋",
-                Snippet = "",
-                Color = "󰏘",
-                File = "󰈙",
-                Reference = "",
-                Folder = "󰉋",
-                EnumMember = "",
-                Constant = "󰏿",
-                Struct = "",
-                Event = "",
-                Operator = "󰆕",
-                TypeParameter = " ",
-            }
+        formatting = {
+          fields = { "kind", "abbr", "menu" },
+          format = function(entry, vim_item)
+            vim_item.menu = ({
+              nvim_lsp = "[LSP]",
+              luasnip = "[Snip]",
+              buffer = "[Buff]",
+              path = "[Path]",
+            })[entry.source.name]
 
-            local buffer_option = {
-                -- Complete from all visible buffers (splits)
-                get_bufnrs = function()
-                    local bufs = {}
-                    for _, win in ipairs(vim.api.nvim_list_wins()) do
-                        bufs[vim.api.nvim_win_get_buf(win)] = true
-                    end
-                    return vim.tbl_keys(bufs)
-                end,
-            }
-            ---@diagnostic disable
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                sources = cmp.config.sources({
-                    {
-                        name = "nvim_lsp",
-                        priority = 10,
-                        entry_filter = limit_lsp_types,
-                    },
-                    {
-                        name = "luasnip",
-                        priority = 7,
-                        max_item_count = 5,
-                    },
-                    {
-                        name = "buffer",
-                        priority = 7,
-                        keyword_length = 5,
-                        max_item_count = 10,
-                        option = buffer_option,
-                    },
-                    { name = "nvim_lua", priority = 5 },
-                    { name = "path", priority = 4 },
-                }),
-                duplicates = {
-                    nvim_lsp = 1,
-                    luasnip = 1,
-                    buffer = 1,
-                    path = 1,
-                },
+            -- for tailwind colors
+            if vim_item.kind == "Color" then
+              vim_item = require("cmp-tailwind-colors").format(entry, vim_item)
+              if kind_icons[vim_item.kind] then
+                vim_item.kind = kind_icons[vim_item.kind] .. " "
+              end
+            end
 
-                formatting = {
-                    fields = { "kind", "abbr", "menu" },
-                    format = function(entry, vim_item)
-                        vim_item.menu = ({
-                            nvim_lsp = "[LSP]",
-                            luasnip = "[Snip]",
-                            buffer = "[Buff]",
-                            path = "[Path]",
-                        })[entry.source.name]
+            -- for codeium
 
-                        -- for tailwind colors
-                        if vim_item.kind == "Color" then
-                            vim_item = require("cmp-tailwind-colors").format(entry, vim_item)
-                            if kind_icons[vim_item.kind] then
-                                vim_item.kind = kind_icons[vim_item.kind] .. " "
-                            end
-                        end
+            vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+            return vim_item
+          end,
+        },
 
-                        -- for codeium
+        experimental = {
+          ghost_text = false,
+          native_menu = false,
+        },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            cmp.config.compare.exact,
+            cmp.config.compare.locality,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.offset,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.order,
+          },
+        },
 
-                        vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-                        return vim_item
-                    end,
-                },
+        mapping = cmp.mapping.preset.insert({
 
-                experimental = {
-                    ghost_text = true,
-                    native_menu = false,
-                },
-                sorting = {
-                    priority_weight = 2,
-                    comparators = {
-                        cmp.config.compare.exact,
-                        cmp.config.compare.locality,
-                        cmp.config.compare.score,
-                        cmp.config.compare.recently_used,
-                        cmp.config.compare.offset,
-                        cmp.config.compare.sort_text,
-                        cmp.config.compare.order,
-                    },
-                },
-
-                mapping = cmp.mapping.preset.insert({
-                    ["<C-n>"] = cmp.mapping(cmp.mapping.scroll_docs(-2), { "i", "c" }),
-                    ["<C-p>"] = cmp.mapping(cmp.mapping.scroll_docs(2), { "i", "c" }),
-                    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-                    ["<C-e>"] = cmp.mapping({
-                        i = cmp.mapping.abort(),
-                        c = cmp.mapping.close(),
-                    }),
-                    ["<CR>"] = cmp.mapping.confirm({
-                        behavior = cmp.ConfirmBehavior.Replace,
-                    }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif cmp.visible() and has_words_before() then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        elseif luasnip.expandable() then
-                            luasnip.expand()
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, {
-                        "i",
-                        "s",
-                    }),
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, {
-                        "i",
-                        "s",
-                    }),
-                }),
-            })
-        end,
-    },
+          ["<Down>"] = cmp.mapping(function(fallback)
+            cmp.close()
+            fallback()
+          end, { "i" }),
+          ["<Up>"] = cmp.mapping(function(fallback)
+            cmp.close()
+            fallback()
+          end, { "i" }),
+          ["<C-n>"] = cmp.mapping(cmp.mapping.scroll_docs(-2), { "i", "c" }),
+          ["<C-p>"] = cmp.mapping(cmp.mapping.scroll_docs(2), { "i", "c" }),
+          ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({
+            i = cmp.mapping.abort(),
+            c = cmp.mapping.close(),
+          }),
+          ["<C-y> "] = cmp.mapping.confirm({
+            behavior = cmp.ConfirmBehavior.Replace,
+          }),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif cmp.visible() and has_words_before() then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+          }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, {
+            "i",
+            "s",
+          }),
+        }),
+      })
+    end,
+  },
 }
--- dotfiles/config/nvim/lua/plugins/codeium.lua
+++ dotfiles/config/nvim/lua/plugins/codeium.lua
@@ -1,20 +1,20 @@
 return {
-    {
-        "Exafunction/codeium.vim",
-        event = "BufEnter",
-        config = function()
-            vim.keymap.set("i", "<C-j>", function()
-                return vim.fn["codeium#Accept"]()
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<C-c>", function()
-                return vim.fn["codeium#CycleCompletions"](1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<C-x>", function()
-                return vim.fn["codeium#CycleCompletions"](-1)
-            end, { expr = true, silent = true })
-            vim.keymap.set("i", "<C-v>", function()
-                return vim.fn["codeium#Clear"]()
-            end, { expr = true, silent = true })
-        end,
-    },
+  {
+    "Exafunction/codeium.vim",
+    config = function()
+      -- Change '<C-g>' here to any keycode you like.
+      vim.keymap.set("i", "<C-j>", function()
+        return vim.fn["codeium#Accept"]()
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-c>", function()
+        return vim.fn["codeium#CycleCompletions"](1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-x>", function()
+        return vim.fn["codeium#CycleCompletions"](-1)
+      end, { expr = true, silent = true })
+      vim.keymap.set("i", "<c-v>", function()
+        return vim.fn["codeium#Clear"]()
+      end, { expr = true, silent = true })
+    end,
+  },
 }
