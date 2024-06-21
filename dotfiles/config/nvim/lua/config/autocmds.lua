2773262d5a4ad053da17f8ac530667f677c17592
jacobadev <junior1232131@gmail.com>
2024-06-21 20:24:12 -0300

    automatic scheduled task

--- dotfiles/config/nvim/lua/config/autocmds.lua
+++ dotfiles/config/nvim/lua/config/autocmds.lua
@@ -3,17 +3,3 @@ vim.api.nvim_create_autocmd(
   { "BufRead", "BufNewFile" },
   { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" }
 )
--- Enable spell checking for certain file types
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.txt", "*.md", "*.tex" }, command = "setlocal spell" }
-)
--- Show `` in specific files
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.txt", "*.md", "*.json" }, command = "setlocal conceallevel=0" }
-)
-local lsp_conficts, _ = pcall(vim.api.nvim_get_autocmds, { group = "LspAttach_conflicts" })
-if not lsp_conficts then
-  vim.api.nvim_create_augroup("LspAttach_conflicts", {})
-end
