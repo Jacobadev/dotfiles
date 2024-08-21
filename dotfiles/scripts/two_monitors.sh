34d6126f58b2da393cb4963357a61785f73ad3f6
jacobadev <junior1232131@gmail.com>
2024-08-21 03:30:04 -0300

    automatic scheduled task

--- dotfiles/scripts/two_monitors.sh
+++ dotfiles/scripts/two_monitors.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
-xrandr --output VGA-0 --primary --rate 60 --mode 1920x1080 --pos 1920x200 --rotate normal --output VGA-1 --off --output HDMI-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-0 --off --output None-1-1 --off
+xrandr --output VGA-0 --primary --rate 60 --mode 1920x1080 --pos 1360x00 --rotate normal --output VGA-1 --off --output HDMI-0 --mode 1360x768 --pos 0x0 --rotate normal --output DVI-D-0 --off --output None-1-1 --off
 sleep 1 
 bspc wm -r
