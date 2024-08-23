32675deb74dee7b3515910da553c0a917206c14a
jacobadev <junior1232131@gmail.com>
2024-08-23 01:36:00 -0300

    automatic scheduled task

--- dotfiles/config/nvim/lua/config/autocmds.lua
+++ dotfiles/config/nvim/lua/config/autocmds.lua
@@ -1,5 +1,5 @@
---
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" }
-)
+----
+--vim.api.nvim_create_autocmd(
+--  { "BufRead", "BufNewFile" },
+--  { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" }
+--)
--- dotfiles/config/nvim/lua/config/keymaps.lua
+++ dotfiles/config/nvim/lua/config/keymaps.lua
@@ -9,33 +9,6 @@ local silent = { silent = true }
 vim.keymap.set("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Prev buffer" })
 vim.keymap.set("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
 
-vim.keymap.set("x", "<leader>re", function()
-  require("refactoring").refactor("Extract Function")
-end)
-vim.keymap.set("x", "<leader>rf", function()
-  require("refactoring").refactor("Extract Function To File")
-end)
--- Extract function supports only visual mode
-vim.keymap.set("x", "<leader>rv", function()
-  require("refactoring").refactor("Extract Variable")
-end)
--- Extract variable supports only visual mode
-vim.keymap.set("n", "<leader>rI", function()
-  require("refactoring").refactor("Inline Function")
-end)
--- Inline func supports only normal
-vim.keymap.set({ "n", "x" }, "<leader>ri", function()
-  require("refactoring").refactor("Inline Variable")
-end)
--- Inline var supports both normal and visual mode
-
-vim.keymap.set("n", "<leader>rb", function()
-  require("refactoring").refactor("Extract Block")
-end)
-vim.keymap.set("n", "<leader>rbf", function()
-  require("refactoring").refactor("Extract Block To File")
-end)
-
 -- Save file by CTRL-S
 keymap("n", "<C-s>", ":w<CR>", silent)
 keymap("i", "<C-s>", "<ESC> :w<CR>i", silent)
@@ -45,17 +18,4 @@ keymap("n", "X", '"_X', silent)
 keymap("v", "x", '"_x', silent)
 keymap("v", "X", '"_X', silent)
 
--- Find word/file across project
-keymap(
-  "n",
-  "<Leader>pf",
-  "<CMD>lua require('plugins.telescope').project_files({ default_text = vim.fn.expand('<cword>'), initial_mode = 'normal' })<CR>"
-)
-keymap("n", "<S-p>", "<CMD>lua require('plugins.telescope.pickers.multi-rg')()<CR>")
-keymap("n", "<Leader>pw", "<CMD>lua require('telescope.builtin').grep_string({ initial_mode = 'normal' })<CR>")
-
-keymap("n", "<C-Space>", "<cmd>lua vim.lsp.buf.code_action()<CR>", silent)
--- vim.keymap.set("n", "<leader>crr", "<cmd>TSToolsFileReferences<cr>", { desc = "Find File References" })
--- vim.keymap.set("n", "<leader>crn", "<cmd>TSToolsRenameFile<cr>", { desc = "Rename File" })
-
-vim.keymap.set("n", "<leader>rr", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "Insert if err" })
+-- vim.keymap.set("n", "<leader>rr", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>", { desc = "Insert if err" })
